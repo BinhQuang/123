f7f26e7b6d623c426de62d4139ea258c4d93cf29
BinhQuang <quangbinh07092004@gmail.com>
2024-12-01 23:40:41 +0700

    Update test8.asm

--- test8.asm
+++ test8.asm
@@ -1,4 +1,18 @@
 li r0, 5    
 shfl r0, r0   
+
 li r1 , 5
 shfr r1 , r1
+
+li r2, 3           
+addi r2, r3, 5     
+subi r2, r4, 2     
+
+li r3, 4           
+and r3, r3, r5    
+or r3, r3, r6      
+xor r3, r3, r7
+    
+mov r3,r4
+mov r0 , r1         
+nop
